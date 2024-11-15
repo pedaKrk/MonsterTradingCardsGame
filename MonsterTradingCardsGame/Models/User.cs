387676bc951135ddffb13e65d371c595b41d7f0a
pedaKrk <peter.karkulik@gmail.com>
2024-11-15 01:30:33 +0100

    remove unnecessary methods

--- MonsterTradingCardsGame/Models/User.cs
+++ MonsterTradingCardsGame/Models/User.cs
@@ -29,53 +29,5 @@ namespace MonsterTradingCardsGame.Models
 
             Coins = 20;
         }
-
-        public void RemoveCoins(int amount)
-        {
-            Coins -= amount;
-        }
-
-        public void BuyPack()
-        {
-            if (Coins - Package.Price < 0)
-            {
-                Console.WriteLine("Not enough Coins to purchase Package!");
-                return;
-            }
-
-            Package package = new Package();
-
-            _stack.AddCards(package.Open());
-        }
-
-        public void AddCardToDeck(Card card)
-        {
-            _deck.AddCard(card);
-        }
-
-        public void RemoveCardFromDeck(Card card)
-        {
-            _deck.RemoveCard(card);
-        }
-
-        public void RemoveCardFromDeckAt(int index)
-        {
-            _deck.RemoveCard(index);
-        }
-
-        public void SwapCardInDeck(Card oldCard, Card newCard)
-        {
-            _deck.SwapCard(oldCard, newCard);
-        }
-
-        public string StackToString()
-        {
-            return _stack.ToString();
-        }
-
-        public string DeckToString()
-        {
-            return _deck.ToString();
-        }
     }
 }
