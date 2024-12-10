ce38c347e3f9c79d38bb35c8dff6ba9c167482b9
Peter Karkulik <peter.karkulik@gmail.com>
2024-12-10 18:50:10 +0100

    refactor model

--- MonsterTradingCardsGame/Models/Deck.cs
+++ MonsterTradingCardsGame/Models/Deck.cs
@@ -8,67 +8,18 @@ namespace MonsterTradingCardsGame.Models
 {
     internal class Deck
     {
-        private readonly List<Card> _cards = [];
+        private List<Card> _cards = [];
 
         public static readonly int DeckSize = 4;
 
-        public int Count { get { return _cards.Count; } }
-
-        public void AddCard(Card card)
+        public void AddCards(List<Card> cards)
         {
-            if (_cards.Count >= DeckSize)
-            {
-                Console.WriteLine("Deck is full!");
-                return;
-            }
-
-            _cards.Add(card);
-        }
-
-        public void RemoveCard(int index)
-        {
-            if (!IsValidIndex(index))
-            {
-                Console.WriteLine("Illegal index!");
-                return;
-            }
-
-            _cards.RemoveAt(index);
-        }
-
-        public void RemoveCard(Card card)
-        {
-            _cards.Remove(card);
+            _cards = new List<Card>(cards);
         }
 
         public List<Card> GetCards()
         {
             return new List<Card>(_cards);
-        }
-
-        private bool IsValidIndex(int index)
-        {
-            return index >= 0 && index < _cards.Count;
-        }
-
-        public override string ToString()
-        {
-            string str = "Deck:\n";
-             
-            for(int i = 0; i < DeckSize; i++)
-            {
-                if(i >= Count)
-                {
-                    str += $"{i}:\n";
-                    continue;
-                }
-
-                str += $"{i}: {_cards[i].ToString()}\n";
-            }
-
-            str += "---------\n\n";
-
-            return str;
         }
     }
 }
