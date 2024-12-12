5b1fd8adcc2c3ecf22f70f5de752e6a40df7367f
Peter Karkulik <=>
2024-12-12 16:35:57 +0100

    program.cs just executes server.run

--- MonsterTradingCardsGame/Program.cs
+++ MonsterTradingCardsGame/Program.cs
@@ -1,16 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using MonsterTradingCardsGame.Models;
 using MonsterTradingCardsGame.Server;
-using System.Net.Sockets;
-using System.Net;
-using System.Text;
 
 Server.Run();
-/*
-User UserA = new User("Player1", "123");
 
-UserA.BuyPack();
-
-Console.WriteLine(UserA.StackToString());
-Console.WriteLine(UserA.DeckToString());
-*/
