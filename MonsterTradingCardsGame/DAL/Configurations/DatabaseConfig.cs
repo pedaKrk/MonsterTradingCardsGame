8edbcadb9200c782ea8e9590f010d520e6bb4196
Peter Karkulik <peter.karkulik@gmail.com>
2025-01-03 13:42:39 +0100

    make properties read only

--- MonsterTradingCardsGame/DAL/Configurations/DatabaseConfig.cs
+++ MonsterTradingCardsGame/DAL/Configurations/DatabaseConfig.cs
@@ -2,9 +2,9 @@
 {
     internal class DatabaseConfig(string host, string database, string username, string password)
     {
-        public string Host { get; set; } = host;
-        public string Database { get; set; } = database;
-        public string Username { get; set; } = username;
-        public string Password { get; set; } = password;
+        public string Host { get; } = host;
+        public string Database { get; } = database;
+        public string Username { get; } = username;
+        public string Password { get; } = password;
     }
 }
