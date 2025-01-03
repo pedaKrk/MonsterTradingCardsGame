9210f125928adad603152330978453c8c0dce28e
Peter Karkulik <peter.karkulik@gmail.com>
2025-01-03 13:42:49 +0100

    remove unused usings

--- MonsterTradingCardsGame/DAL/Services/DatabaseConfigService.cs
+++ MonsterTradingCardsGame/DAL/Services/DatabaseConfigService.cs
@@ -1,7 +1,5 @@
-﻿
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using MonsterTradingCardsGame.DAL.Configurations;
-using System.Text.Json;
 
 namespace MonsterTradingCardsGame.DAL.Services
 {
